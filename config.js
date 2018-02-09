d727384c654a1cd3698bf295ccb647136d5be295
Dobi Okrasa <dobi.okrasa@gmail.com>
2018-02-09 15:20:24 +0100

    Add scripts

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID': '1067122618059-gnb5l8dn69v4ct220d56gv6ko2c9va22.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': '6CrZW9jQxlZwgE2YNDnd9J3h',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
